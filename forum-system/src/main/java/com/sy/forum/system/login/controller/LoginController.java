c4450f3d4fadee8cd8d734bb88481864d030874a
Carl <carl_email_96@163.com>
2017-04-20 22:15:44 +0800

    小部分修改

--- forum-system/src/main/java/com/sy/forum/system/login/controller/LoginController.java
+++ forum-system/src/main/java/com/sy/forum/system/login/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.sy.forum.system.login.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @Author SY
@@ -10,14 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: TODO(这里用一句话描述这个类的作用)
  * @Date 2017-04-20 10:24
  */
-@RestController
+@Controller
 @RequestMapping("/login")
 public class LoginController {
 
     @RequestMapping("/initLoginPage")
     public String initLoginPage(Model model) {
         model.addAttribute("hello", "Hello, My name is Carl! What't you name?");
-        System.out.println("Carl init login page");
-        return "/login/login";
+        System.out.println("Carl init main page");
+        return "index";
     }
 }
